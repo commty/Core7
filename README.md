06e4fee94308c58b642a748109ffea3c2eabe94e
Happensit <antonybizov@gmail.com>
2014-10-11 01:32:47 +0400

    Add class img-responsive for user_picture

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Core 7 CMF
 Fork of Drupal 7
 
 New folder structure as drupal 8
+Html emails by default
--- core/modules/overlay/overlay.tpl.php
+++ core/modules/overlay/overlay.tpl.php
@@ -27,14 +27,13 @@
   <div id="overlay-titlebar" class="clearfix">
     <div id="overlay-title-wrapper" class="clearfix">
       <h1 id="overlay-title"<?php print $title_attributes; ?>><?php print $title; ?></h1>
-      <div id="overlay-close-wrapper">
-        <a id="overlay-close" href="#" class="overlay-close"></a>
-      </div>
     </div>
-
-    <?php if ($tabs): ?><h2 class="element-invisible"><?php print t('Primary tabs'); ?></h2><ul id="overlay-tabs"><?php print render($tabs); ?></ul><?php endif; ?>
+    <div id="overlay-close-wrapper">
+      <a id="overlay-close" href="#" class="overlay-close"><span class="element-invisible"><?php print t('Close overlay'); ?></span></a>
+    </div>
   </div>
   <div id="overlay-content"<?php print $content_attributes; ?>>
     <?php print $page; ?>
+      <a class="footer-brand"  href="http://softformul.com">Softformula - Разработка уникальных интернет-проектов</a>
   </div>
 </div>
