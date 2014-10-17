d432919f869ae11ff8f235f11997873b7ee011ab
Happensit <antonybizov@gmail.com>
2014-10-17 17:26:43 +0400

    Autoupload image

---
+++ core/modules/image/autosubmit.js
@@ -0,0 +1,19 @@
+/*
+ * Behavior for the automatic file upload
+ */
+
+(function ($) {
+    Drupal.behaviors.image = {
+        attach: function(context) {
+            $('.form-item input.form-submit[value= Drupal.t("Upload")]', context).hide();
+            $('.form-item input.form-file', context).change(function() {
+                $parent = $(this).closest('.form-item');
+                setTimeout(function() {
+                    if(!$('.error', $parent).length) {
+                        $('input.form-submit[value=Drupal.t("Upload")]', $parent).mousedown();
+                    }
+                }, 100);
+            });
+        }
+    };
+})(jQuery);
