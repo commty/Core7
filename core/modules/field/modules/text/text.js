8211bc09ef93cf7c34e3547091fc068f94a22420
Happensit <antonybizov@gmail.com>
2014-11-09 05:08:48 +0300

    release 7.33

--- core/modules/field/modules/text/text.js
+++ core/modules/field/modules/text/text.js
@@ -1,49 +1,53 @@
 
 (function ($) {
 
-/**
- * Auto-hide summary textarea if empty and show hide and unhide links.
- */
-Drupal.behaviors.textSummary = {
-  attach: function (context, settings) {
-    $('.text-summary', context).once('text-summary', function () {
-      var $widget = $(this).closest('div.field-type-text-with-summary');
-      var $summaries = $widget.find('div.text-summary-wrapper');
+    /**
+     * Auto-hide summary textarea if empty and show hide and unhide links.
+     */
+    Drupal.behaviors.textSummary = {
+        attach: function (context, settings) {
+            $('.text-summary', context).once('text-summary', function () {
+                var $widget = $(this).closest('div.field-type-text-with-summary');
+                var $summaries = $widget.find('div.text-summary-wrapper');
 
-      $summaries.once('text-summary-wrapper').each(function(index) {
-        var $summary = $(this);
-        var $summaryLabel = $summary.find('label');
-        var $full = $widget.find('.text-full').eq(index).closest('.form-item');
-        var $fullLabel = $full.find('label');
+                $summaries.once('text-summary-wrapper').each(function(index) {
+                    var $summary = $(this);
+                    var $summaryLabel = $summary.find('label').first();
+                    var $full = $widget.find('.text-full').eq(index).closest('.form-item');
+                    var $fullLabel = $full.find('label').first();
 
-        // Create a placeholder label when the field cardinality is
-        // unlimited or greater than 1.
-        if ($fullLabel.length == 0) {
-          $fullLabel = $('<label></label>').prependTo($full);
+                    // Create a placeholder label when the field cardinality is
+                    // unlimited or greater than 1.
+                    if ($fullLabel.length == 0) {
+                        $fullLabel = $('<label></label>').prependTo($full);
+                    }
+
+                    // Setup the edit/hide summary link.
+                    var $link = $('<span class="field-edit-link">(<a class="link-edit-summary" href="#">' + Drupal.t('Hide summary') + '</a>)</span>');
+                    var $a = $link.find('a');
+                    var toggleClick = true;
+                    $link.bind('click', function (e) {
+                        if (toggleClick) {
+                            $summary.hide();
+                            $a.html(Drupal.t('Edit summary'));
+                            $link.appendTo($fullLabel);
+                        }
+                        else {
+                            $summary.show();
+                            $a.html(Drupal.t('Hide summary'));
+                            $link.appendTo($summaryLabel);
+                        }
+                        toggleClick = !toggleClick;
+                        return false;
+                    }).appendTo($summaryLabel);
+
+                    // If no summary is set, hide the summary field.
+                    if ($(this).find('.text-summary').val() == '') {
+                        $link.click();
+                    }
+                });
+            });
         }
-
-        // Setup the edit/hide summary link.
-        var $link = $('<span class="field-edit-link"><a class="link-edit-summary" href="#">Скрыть тизер</a></span>').toggle(
-          function () {
-            $summary.hide();
-            $(this).find('a').html('Редактировать тизер').end().appendTo($fullLabel);
-            return false;
-          },
-          function () {
-            $summary.show();
-            $(this).find('a').html('Скрыть тизер').end().appendTo($summaryLabel);
-            return false;
-          }
-        ).appendTo($summaryLabel);
-
-        // If no summary is set, hide the summary field.
-        if ($(this).find('.text-summary').val() == '') {
-          $link.click();
-        }
-        return;
-      });
-    });
-  }
-};
+    };
 
 })(jQuery);
